51857341b2e452ba0105a29151a69b84c2ab181c
Harshneet <harshneets50@gmail.com>
2022-07-12 15:58:40 +0530

    more work on project 10th

--- scroll-smooth/app.js
+++ scroll-smooth/app.js
@@ -2,8 +2,13 @@ const togglebtn = document.querySelector('.toggle');
 const link = document.querySelector(".links")
 
 const linkContainer = document.querySelector(".linksContainer")
-//************** */ navbar ****************
+
+
+//**************  navbar ****************
+
+
 togglebtn.addEventListener("click", function () {
+
     const linksConatinerHeight = linkContainer.getBoundingClientRect().height
 
     const linksHeight = link.getBoundingClientRect().height
@@ -21,12 +26,17 @@ togglebtn.addEventListener("click", function () {
     }
 
 })
+
+
 // *************fixed nav*******************
+
+
 const nav = document.querySelector(".hello");
 
 window.addEventListener('scroll', function () {
+
     const scrollHeight = window.pageYOffset;
-    const topLink=document.querySelector('.top-link')
+    const topLink = document.querySelector('.top-link')
     const navHeight = nav.getBoundingClientRect().height;
     if (scrollHeight > navHeight) {
         nav.classList.add('fixed-nav');
@@ -37,17 +47,61 @@ window.addEventListener('scroll', function () {
     }
 
     // ******* top links*******
-    
-    if (scrollHeight>500) {
+    if (scrollHeight > 500) {
         topLink.classList.add('show-link');
-    }else{
+    } else {
         topLink.classList.remove('show-link');
 
     }
 
 })
+// ******* smooth scroll********************
+
+//  select links
+const scrollTo = document.querySelectorAll('.scroll-to');
+
+
+scrollTo.forEach(element => {
+    element.addEventListener('click', function (e) {
+        //  *  now preventing for scrolling which is automatic in html 
+
+        e.preventDefault();
+
+        // *  now target the click and taking its attribute href value and removing # from it
+
+        const target = e.currentTarget.getAttribute('href').slice(1)
+        // console.log(target)
+
+        //* now getting the target href id 
+
+        const id = document.getElementById(target);
+        // console.log(id);
+
+        //* now finding the id position from the top
+        // * we are - the navHeight as we want out section title display below the navbar
+
+
+        const navHeight = nav.getBoundingClientRect().height;
+        
+        const linksHeight = link.getBoundingClientRect().height
+        let position = id.offsetTop - navHeight;
+        if (navHeight > 83) {
+            position = position + linksHeight;
+        }
+
+        //* here we are scrolling to the position 
+        // * scrollto(x,y) here x means horizantly scrolling and y means vertically scrolling
+
+        window.scrollTo(0, position)
+
+        // *here were bringing linkscontainer for RESPONSIVE DESIGN and making its height 0 so that as person click the e then its height will go to 0 
+
+        linkContainer.style.height = 0;
+    })
+});
 
 //***************  footer date*******************
+
 
 const date = document.getElementById("date")
 
